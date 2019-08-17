115f3c6ec591fb4903db26243e66266ab213469a
kazurayam <kazuaki.urayama@gmail.com>
2019-08-17 20:59:39 +0900

    fixed TL1.groovy

--- Test Listeners/TL1.groovy
+++ Test Listeners/TL1.groovy
@@ -5,6 +5,7 @@ import com.kms.katalon.core.util.KeywordUtil
 import java.time.format.DateTimeFormatter
 import java.time.LocalDateTime
 import internal.GlobalVariable as GlobalVariable
+import com.kms.katalon.core.exception.StepFailedException
 
 class TL1 {
 	/**
@@ -13,18 +14,41 @@ class TL1 {
 	 */
 	@BeforeTestSuite
 	def beforeTestSuite(TestSuiteContext testSuiteContext) {
-		KeywordUtil.metaClass.markPassed = { String message ->
+		KeywordUtil.metaClass.static.markPassed = { String message ->
 			// insert whatever lines of processing you want
 			LocalDateTime localDateTime = LocalDateTime.now()
 			GlobalVariable.TimeStamp = localDateTime.format(
 				DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS"))
-			println "Timestamp=${GlobalVariable.TimeStamp}"
+			println "in markPassed Timestamp=${GlobalVariable.TimeStamp}"
 			
 			// the following 2 lines are copy&pasted from the original implementation
 			logger.logPassed(message);
 			ErrorCollector.getCollector().setKeywordPassed(true);
 		
 			// insert whatever lines of processing you want
+		}
+		
+		KeywordUtil.metaClass.static.markFailed = { String message ->
+			// additive
+			LocalDateTime localDateTime = LocalDateTime.now()
+			GlobalVariable.TimeStamp = localDateTime.format(
+				DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS"))
+			println "in markFailed Timestamp=${GlobalVariable.TimeStamp}"
+			
+			// the original implementation
+			ErrorCollector.getCollector().addError(new StepFailedException(message))
+		}
+		
+		KeywordUtil.metaClass.static.markFailedAndStop = { String message ->
+			// additive
+			LocalDateTime localDateTime = LocalDateTime.now()
+			GlobalVariable.TimeStamp = localDateTime.format(
+				DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss.SSS"))
+			println "in markupFailedAndStop Timestamp=${GlobalVariable.TimeStamp}"
+			
+			// the original implementation
+			throw new StepFailedException(message)
+			
 		}
 	}
 }
